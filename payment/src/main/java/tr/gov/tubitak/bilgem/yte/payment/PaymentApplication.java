7420b77535f658e236961eb9aad5690cee326bc1
baransonmez <baran.sonmez@tubitak.gov.tr>
2023-11-11 19:08:56 +0300

    payment servisi oluşturuldu.

--- payment/src/main/java/tr/gov/tubitak/bilgem/yte/payment/PaymentApplication.java
+++ payment/src/main/java/tr/gov/tubitak/bilgem/yte/payment/PaymentApplication.java
@@ -1,13 +1,40 @@
 package tr.gov.tubitak.bilgem.yte.payment;
 
-import org.springframework.boot.SpringApplication;
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.TimeUnit;
+
+enum Status {
+    SUCCESS,
+    FAILED
+}
 
 @SpringBootApplication
 public class PaymentApplication {
+    private static final String DAPR_STATE_STORE = "statestore";
 
-    public static void main(String[] args) {
-        SpringApplication.run(PaymentApplication.class, args);
+    public static void main(String[] args) throws Exception {
+        try (DaprClient client = new DaprClientBuilder().build()) {
+            for (int i = 1; i <= 16; i++) {
+                Payment payment = new Payment();
+                payment.setOrderId(i);
+                payment.setStatus(i % 4 == 0 ? Status.FAILED : Status.SUCCESS);
+                // Save state into the state store
+                client.saveState(DAPR_STATE_STORE, String.valueOf(i), payment).block();
+                System.out.println("Saving Payment: " + payment.getOrderId());
+                TimeUnit.MILLISECONDS.sleep(1000);
+            }
+        }
     }
+}
 
+@Getter
+@Setter
+class Payment {
+    private int orderId;
+    private Status status;
 }
